facbd0a6d29aeda1211920a9c769cfcec1f3260e
Hugo Voisin <hugov@bam.tech>
2020-01-02 23:57:05 +0100

    test(musicList): add button press test

--- src/pages/MusicList/__tests__/MusicList.test.js
+++ src/pages/MusicList/__tests__/MusicList.test.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import {render} from 'react-native-testing-library';
+import {render, fireEvent} from 'react-native-testing-library';
 import {MusicList} from '../MusicList';
 import {getPropsWithNavigation} from '../../../lib/tests/helpers';
 
+const song0Id = 0;
+const song0Title = 'toto';
 jest.mock('../../../data/index', () => ({
   '0': {id: 0, title: 'toto'},
   '1': {id: 1, title: 'tata'},
 }));
 
-const navigation = getPropsWithNavigation();
+const navigate = jest.fn();
+const {navigation} = getPropsWithNavigation({}, {navigate});
 
 describe('MusicItem', () => {
   it('should render', () => {
-    const musicItem = render(<MusicList navigation={navigation} />);
+    const musicList = render(<MusicList navigation={navigation} />);
 
-    expect(musicItem).toMatchSnapshot();
+    expect(musicList).toMatchSnapshot();
+  });
+  it('should navigate to lyric page', () => {
+    const {getByText} = render(<MusicList navigation={navigation} />);
+
+    const itemButton = getByText(song0Title);
+    fireEvent.press(itemButton);
+
+    expect(navigate).toHaveBeenCalledWith('MusicView', {musicId: song0Id});
   });
 });
